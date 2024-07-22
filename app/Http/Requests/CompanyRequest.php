bd3201258abc5071735311faf51ea5664d1d9ec7
Юра Гуцалюк <hutsaluck@gmail.com>
2024-07-22 14:42:40 +0300

    Remove unique for email

--- app/Http/Requests/CompanyRequest.php
+++ app/Http/Requests/CompanyRequest.php
@@ -23,7 +23,7 @@ class CompanyRequest extends FormRequest
     {
         return [
             'name'       => [ 'required', 'string', 'max:255' ],
-            'email'      => [ 'required', 'email', 'unique:companies' ],
+            'email'      => [ 'required', 'email' ],
             'country_id' => [ 'required', 'exists:countries,id' ],
         ];
     }
